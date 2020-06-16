037f9e2f21bc91815d3aa024e92274716ddfe20d
Ger Hobbelt <ger@hobbelt.com>
2020-06-16 21:34:04 +0200

    - updated npm packages - synced with template markdown-it plugin build tooling files - rebuilt library files

--- dist/markdownItAsciiMath.mjs
+++ dist/markdownItAsciiMath.mjs
@@ -1,6 +1,9 @@
+/*! markdown-it-asciimath 0.1.0-1 https://github.com//GerHobbelt/markdown-it-asciimath @license MIT */
 
 let katex = require('katex');
+
 let assign = require('lodash.assign');
+
 let defaults = {
   useKeyword: false
 };
@@ -9,11 +12,10 @@ function setup(md, options) {
   if (typeof options === 'undefined') {
     options = defaults;
   }
+
   let useKeyword = options.useKeyword;
-  console.log(useKeyword);
+  console.log(useKeyword); //var options = assign({}, defaults, options);
 
-
-  //var options = assign({}, defaults, options);
   let defaultRender = md.renderer.rules.fence;
 
   md.renderer.rules.fence = function (tokens, idx, options, env, self) {
@@ -25,28 +27,28 @@ function setup(md, options) {
 
     if (token.info === 'latex') {
       return renderTeX(token.content, true);
-    }
+    } // pass token to default renderer.
 
-    // pass token to default renderer.
+
     return defaultRender(tokens, idx, options, env, self);
   };
 
   md.renderer.rules.code_inline = function (tokens, idx, options, env, self) {
     let token = tokens[idx];
-
     console.log(useKeyword);
 
     if (!useKeyword) {
       console.log('1');
       return renderInline(token.content.trim(), false);
     }
+
     console.log('2');
+
     if (token.content.substr(0, 4) === 'math') {
       return renderInline(token.content.substr(4).trim(), false);
     } else if (token.content.substr(0, 5) === 'latex') {
       return renderElement(token.content.substr(5).trim(), false);
     }
-
 
     return defaultRender(tokens, idx, options, env, self);
   };
@@ -55,9 +57,8 @@ function setup(md, options) {
 function render(str, disp) {
   // split content
   let arr = str.trim().split('\n');
-  let result = '';
+  let result = ''; // render each line, skipping empty lines
 
-  // render each line, skipping empty lines
   for (let i = 0; i < arr.length; i++) {
     if (arr[i]) {
       result += '<p>' + renderElement(preprocessMath(arr[i]), disp) + '<p>';
@@ -70,9 +71,8 @@ function render(str, disp) {
 function renderTeX(str, disp) {
   // split content
   let arr = str.trim().split('\n');
-  let result = '';
+  let result = ''; // render each line, skipping empty lines
 
-  // render each line, skipping empty lines
   for (let i = 0; i < arr.length; i++) {
     if (arr[i]) {
       result += '<p>' + renderElement(arr[i], disp) + '<p>';
@@ -87,19 +87,19 @@ function renderInline(str, disp) {
 }
 
 function renderElement(str, disp) {
-  return katex.renderToString(str, { displayMode: disp });
+  return katex.renderToString(str, {
+    displayMode: disp
+  });
 }
 
 function preprocessMath(str) {
-  let newstr;
+  let newstr; // correct index-texts
 
-  // correct index-texts
-  newstr = str.replace(/_(.*?)(\s|$|=|\(|\)|\*|\/|\^)/g, '_($1)$2');
+  newstr = str.replace(/_(.*?)(\s|$|=|\(|\)|\*|\/|\^)/g, '_($1)$2'); // parse to TeX
 
-  // parse to TeX
   newstr = AMTparseAMtoTeX(newstr);
-
   return newstr;
 }
 
 module.exports = setup;
+//# sourceMappingURL=markdownItAsciiMath.mjs.map
---
+++ dist/markdownItAsciiMath.umd.js
@@ -0,0 +1,112 @@
+/*! markdown-it-asciimath 0.1.0-1 https://github.com//GerHobbelt/markdown-it-asciimath @license MIT */
+
+(function (factory) {
+  typeof define === 'function' && define.amd ? define(factory) :
+  factory();
+}((function () { 'use strict';
+
+  let katex = require('katex');
+
+  let assign = require('lodash.assign');
+
+  let defaults = {
+    useKeyword: false
+  };
+
+  function setup(md, options) {
+    if (typeof options === 'undefined') {
+      options = defaults;
+    }
+
+    let useKeyword = options.useKeyword;
+    console.log(useKeyword); //var options = assign({}, defaults, options);
+
+    let defaultRender = md.renderer.rules.fence;
+
+    md.renderer.rules.fence = function (tokens, idx, options, env, self) {
+      let token = tokens[idx];
+
+      if (token.info === 'math') {
+        return render(token.content, true);
+      }
+
+      if (token.info === 'latex') {
+        return renderTeX(token.content, true);
+      } // pass token to default renderer.
+
+
+      return defaultRender(tokens, idx, options, env, self);
+    };
+
+    md.renderer.rules.code_inline = function (tokens, idx, options, env, self) {
+      let token = tokens[idx];
+      console.log(useKeyword);
+
+      if (!useKeyword) {
+        console.log('1');
+        return renderInline(token.content.trim(), false);
+      }
+
+      console.log('2');
+
+      if (token.content.substr(0, 4) === 'math') {
+        return renderInline(token.content.substr(4).trim(), false);
+      } else if (token.content.substr(0, 5) === 'latex') {
+        return renderElement(token.content.substr(5).trim(), false);
+      }
+
+      return defaultRender(tokens, idx, options, env, self);
+    };
+  }
+
+  function render(str, disp) {
+    // split content
+    let arr = str.trim().split('\n');
+    let result = ''; // render each line, skipping empty lines
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]) {
+        result += '<p>' + renderElement(preprocessMath(arr[i]), disp) + '<p>';
+      }
+    }
+
+    return result;
+  }
+
+  function renderTeX(str, disp) {
+    // split content
+    let arr = str.trim().split('\n');
+    let result = ''; // render each line, skipping empty lines
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]) {
+        result += '<p>' + renderElement(arr[i], disp) + '<p>';
+      }
+    }
+
+    return result;
+  }
+
+  function renderInline(str, disp) {
+    return renderElement(preprocessMath(str), disp);
+  }
+
+  function renderElement(str, disp) {
+    return katex.renderToString(str, {
+      displayMode: disp
+    });
+  }
+
+  function preprocessMath(str) {
+    let newstr; // correct index-texts
+
+    newstr = str.replace(/_(.*?)(\s|$|=|\(|\)|\*|\/|\^)/g, '_($1)$2'); // parse to TeX
+
+    newstr = AMTparseAMtoTeX(newstr);
+    return newstr;
+  }
+
+  module.exports = setup;
+
+})));
+//# sourceMappingURL=markdownItAsciiMath.umd.js.map
